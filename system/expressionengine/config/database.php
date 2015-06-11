75cf6f94e92828407e93b0cc4be5d03b604f4f75
alejandrogarciasalas <garciasalasalejandro@gmail.com>
2015-06-11 15:14:41 -0500

    added system folder

---
+++ system/expressionengine/config/database.php
@@ -0,0 +1,22 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$active_group = 'expressionengine';
+$active_record = TRUE;
+
+$db['expressionengine']['hostname'] = 'localhost';
+$db['expressionengine']['username'] = 'laboratoria_r';
+$db['expressionengine']['password'] = 'Lab0r@t0riA2014';
+$db['expressionengine']['database'] = 'laboratoria_db';
+$db['expressionengine']['dbdriver'] = 'mysqli';
+$db['expressionengine']['pconnect'] = FALSE;
+$db['expressionengine']['dbprefix'] = 'exp_';
+$db['expressionengine']['swap_pre'] = 'exp_';
+$db['expressionengine']['db_debug'] = TRUE;
+$db['expressionengine']['cache_on'] = FALSE;
+$db['expressionengine']['autoinit'] = FALSE;
+$db['expressionengine']['char_set'] = 'utf8';
+$db['expressionengine']['dbcollat'] = 'utf8_general_ci';
+$db['expressionengine']['cachedir'] = '/Users/alejandro/Sites/laboratoria-v2/system/expressionengine/cache/db_cache/';
+
+/* End of file database.php */
+/* Location: ./system/expressionengine/config/database.php */
